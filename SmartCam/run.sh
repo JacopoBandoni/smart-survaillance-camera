4c65c01a781576a571b7b238edfb0263cddf4548
GBisi <giuseppe.bisicchia@outlook.it>
2021-05-05 20:53:44 +0200

    run.sh

--- SmartCam/run.sh
+++ SmartCam/run.sh
@@ -5,7 +5,10 @@ case "$1" in
         pip3 install -r requirements.txt
         ;;
     "exp")
-        python3 probe.py 0.25 1200 f probe-report.txt probe.analysis.txt
+        python3 src/app.py &
+        sleep 5
+        python3 probe.py 0.25 12 f probe-report.txt probe.analysis.txt
+        pkill -f "python3"
         ;;
     "")
         python3 src/app.py
