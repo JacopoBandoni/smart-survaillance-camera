0f4d83b36854113578cf2148c30a176f7c0d4da5
GBisi <giuseppe.bisicchia@outlook.it>
2021-05-07 17:19:35 +0200

    test

--- SmartCam/run.sh
+++ SmartCam/run.sh
@@ -15,48 +15,48 @@ case "$1" in
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe1.report.txt experiments/probe1.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry1 ./experiments/frames_size.txt
+        rm -r ./frames-raspberry1
         sleep 300
         
         python3 src/app1O.py &
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe1O.report.txt experiments/probe1O.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry1o ./experiments/frames_size.txt
+        rm -r ./frames-raspberry1o
         sleep 300
 
         python3 src/app2.py &
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe2.report.txt experiments/probe2.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry2 ./experiments/frames_size.txt
+        rm -r ./frames-raspberry2
         sleep 300
 
         python3 src/app2O.py &
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe2O.report.txt experiments/probe2O.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry2o ./experiments/frames_size.txt
+        rm -r ./frames-raspberry2o
         sleep 300
 
         python3 src/app3.py &
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe3.report.txt experiments/probe3.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry3 ./experiments/frames_size.txt
+        rm -r ./frames-raspberry3
         sleep 300
 
         python3 src/app3O.py &
         sleep 5
         python3 probe.py 0.25 1200 f experiments/probe3O.report.txt experiments/probe3O.analysis.txt
         pkill -f "python3"
-        python3 frames_size.py ./frames-raspberry ./experiments/frames_size.txt
-        rm -r ./frames-raspberry
+        python3 frames_size.py ./frames-raspberry3o ./experiments/frames_size.txt
+        rm -r ./frames-raspberry3o
     
         ;;
     "")
